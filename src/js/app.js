6ec98dd89b80471de52279ad4c6d8ec0468df0bc
Vitali Skarbinski <sfc-ruper@mail.ru>
2023-04-24 17:10:16 +0300

    init: start roofing landing page

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as webpCheck from "./modules/webpcheck.js"
+import * as burgerMenu from "./modules/burgermenu.js"
+//import * as tabMenu from "./modules/tabs.js"
+//import * as Swiper from "./modules/swiper.js"
+
+webpCheck.isWebp()
+burgerMenu.burger()
+//tabMenu.tabs()
+//Swiper.Swiper()
---
+++ src/js/modules/swiper.js
@@ -0,0 +1,22 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const Swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
