104616d991718cc8f967304ee9efeb1ee5ca2a10
Vitali Skarbinski <sfc-ruper@mail.ru>
2023-04-25 18:06:54 +0300

    feat: add home section and responsive. add switch dark / lights theme to web page

--- src/js/modules/themebtn.js
+++ src/js/modules/themebtn.js
@@ -1,6 +1,23 @@
 export function themeChange() {
   const themeBtn = document.querySelector('.theme-btn')
+  const wrapper = document.querySelector('.wrapper')
+  const pageBtns = document.querySelector('.button')
+  const logos = document.querySelector('.svg-logo')
+  const links = document.querySelectorAll('.links')
+  const navigation = document.querySelector('.navigation')
+  const burgerSpan = document.querySelectorAll('.burger__icon span')
   themeBtn.addEventListener('click', () => {
+    wrapper.classList.toggle('lights')
     themeBtn.classList.toggle('active')
+    pageBtns.classList.toggle('lights')
+    navigation.classList.toggle('lights')
+    logos.classList.toggle('lights')
+    links.forEach(link => {
+      link.classList.toggle('lights')
+    })
+    burgerSpan.forEach(span => {
+      span.classList.toggle('lights')
+    })
+
   })
 }
