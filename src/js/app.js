c21bc36f36f628c66079764f9d4c8ecc3d993e01
Vitali Skarbinski <sfc-ruper@mail.ru>
2023-05-18 05:33:32 +0300

    add animation on scroll

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import * as webpCheck from "./modules/webpcheck.js"
 import * as burgerMenu from "./modules/burgermenu.js"
 import * as themeButton from "./modules/themebtn.js"
 import * as readMore from "./modules/readmore.js"
+import * as scroll from "./modules/animateonscroll.js"
 
 //import * as tabMenu from "./modules/tabs.js"
 import * as Swiper from "./modules/swiper.js"
@@ -10,6 +11,7 @@ webpCheck.isWebp()
 burgerMenu.burger()
 themeButton.themeChange()
 readMore.readMoreFunc()
+scroll.scrollAnimation()
 
 //tabMenu.tabs()
 Swiper.swiper()
