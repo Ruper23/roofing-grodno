e0ef69b38d1d134afbb8c41505349a0c9a1dfc46
Vitali Skarbinski <sfc-ruper@mail.ru>
2023-06-02 04:59:26 +0300

    change axios method post

--- src/js/modules/tgalert.js
+++ src/js/modules/tgalert.js
@@ -1,9 +1,9 @@
 export function tgalert() {
   const tgToken = '6019123175:AAE-obC5pgJBLiLIxDzvqC9F2j_gbK2VEFk'
   const tgChatId = '-1001836439135'
-  const tgUriApi = `https://api.telegram.org/bot${tgToken}/sendMessage`
+  const tgUriApi = `https://api.telegram.org/bot${ tgToken }/sendMessage`
 
-  const contactsForm = document.querySelector('.contacts__form')
+  const contactsForm = document.getElementById('tg')
   const sendSuccess = document.querySelector('.send-success')
   const showAlert = () =>{
     sendSuccess.style.opacity = '1'
@@ -16,17 +16,17 @@ export function tgalert() {
     e.preventDefault()
     let tgMessage = `<b>Заявка с сайта</b> \n`
     tgMessage += `<b>Отправитель:</b> ${this.name.value}</b>\n`
-    tgMessage += `<b>Номер телефона:</b> ${this.number.value}</b>\n`
-    tgMessage += `<b>Сообщение:</b> ${this.message.value}</b>\n`
+    tgMessage += `<b>Номер телефона:</b> ${this.phone.value}</b>\n`
+    tgMessage += `<b>Сообщение:</b> ${this.message.value}</b>`
 
-    axios.post(tgUriApi,{
+    axios.post(tgUriApi, {
       chat_id: tgChatId,
       parse_mode: 'html',
       text: tgMessage,
     })
-    .then((res) =>{
+    .then((response) =>{
       this.name.value = ''
-      this.number.value = ''
+      this.phone.value = ''
       this.message.value = ''
       showAlert()
     })
